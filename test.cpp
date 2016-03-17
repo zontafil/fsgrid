5f1210b70f508a53fb4c85b6fc7c999b83451756
Sebastian von Alfthan <sebastian.von.alfthan@csc.fi>
2016-03-17 13:33:54 +0200

    Added test for ghost update

--- test.cpp
+++ test.cpp
@@ -13,7 +13,6 @@ int main(int argc, char** argv) {
    std::array<int, 3> isPeriodic{false,false,true};
    {
       FsGrid<int,1> testGrid(globalSize, MPI_COMM_WORLD, isPeriodic);
-
       if(rank == 0) {
          for(int i=0; i<8; i++) {
             auto taskLid = testGrid.getTaskForGlobalID(8*1*0+8*0+i);
@@ -24,16 +23,65 @@ int main(int argc, char** argv) {
          for(int i=0; i<8; i++) {
             auto taskLid = testGrid.getTaskForGlobalID(8*1*0+8*i+0);
             std::cerr << "Cell ( " << 0 << ", " << i << ", 0) is located on task "
-               << taskLid.first << std::endl;
+                      << taskLid.first << std::endl;
             std::cerr << "   and it has LocalID " << taskLid.second << std::endl;
          }
          for(int i=0; i<8; i++) {
             auto taskLid = testGrid.getTaskForGlobalID(8*1*0+8*i+i);
             std::cerr << "Cell ( " << i << ", " << i << ", 0) is located on task "
-               << taskLid.first << std::endl;
+                      << taskLid.first << std::endl;
          }
       }
+      
+      std::array<int, 3>  localSize=testGrid.getLocalSize();
+
+
+      
+      for(int z = 0; z < localSize[2]; z++){
+         for(int y = 0; y < localSize[1]; y++){
+            for(int x = 0; x < localSize[0]; x++){
+               testGrid.get(x, y, z) = rank;
+            }
+         }
+      }
+      
+      
+      if(rank==0) {
+         printf("local size %d %d %d\n", localSize[0], localSize[1], localSize[2]);
+         printf("----------------------------------\n");
+         for(int z = -1; z < localSize[2] +1; z++){
+            printf("z=%d\n", z);
+            for(int y = -1; y < localSize[1] + 1; y++){
+               printf("y=%d :", y);
+               for(int x = -1; x < localSize[0] +1;  x++){
+                  printf("%d ", testGrid.get(x, y, z));
+               }
+               printf("\n");
+            }
+         }
+         printf("----------------------------------\n");
+      }
+      
+
+      testGrid.updateGhostCells();
+
+      if(rank==0) {
+         printf("local size %d %d %d\n", localSize[0], localSize[1], localSize[2]);
+         printf("----------------------------------\n");
+         for(int z = -1; z < localSize[2] +1; z++){
+            for(int y = -1; y < localSize[1] + 1; y++){
+               for(int x = -1; x < localSize[0] +1;  x++){
+                  printf("%d ", testGrid.get(x, y, z));
+               }
+               printf("\n");
+            }
+         }
+         printf("----------------------------------\n");
+      }
    }
+   
+
+   
    MPI_Finalize();
 
    return 0;
