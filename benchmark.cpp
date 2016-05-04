d619c678a63e2f5cdf712a05061957a79a77c76a
Sebastian von Alfthan <sebastian.von.alfthan@csc.fi>
2016-05-04 14:58:13 +0300

    Added license

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include "fsgrid.hpp"
+/*
+  Copyright (C) 2016 Finnish Meteorological Institute
+  Copyright (C) 2016 CSC -IT Center for Science 
+
+  This file is part of fsgrid
+ 
+  fsgrid is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  lib-Slice3D is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY;
+  without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with fsgrid.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 template<class T, int stencil> void timeit(std::array<int32_t, 3> globalSize, std::array<int32_t, 3> isPeriodic, int iterations){
    double t1,t2;   
